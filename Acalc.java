33fe42b75fc440a6dd338453adcbe1869761660b
abhijith2k7 <abhijeeth4232@gmail.com>
2026-03-26 14:54:19 +0530

    Acalc.java

--- Acalc.java
+++ Acalc.java
@@ -1,21 +1,15 @@
 class Calculator {
-
-    // public method - 2 integers
     public int add(int a, int b) {
         return a + b;
     }
-
-    // protected method - 3 integers
     protected int add(int a, int b, int c) {
         return a + b + c;
     }
-
-    // private method - 2 decimal numbers
     private double add(double a, double b) {
         return a + b;
     }
 
-    // public method calling private method
+  
     public void showDecimalSum(double a, double b) {
         double result = this.add(a, b);   // calling private method
         System.out.println("Decimal Sum: " + result);
